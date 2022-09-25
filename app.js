d013db423d5458667d272d4da421b43c6fa3bdd9
XujuanChen <xchen022@citymail.cuny.edu>
2022-09-25 19:44:54 -0400

    8080

--- app.js
+++ app.js
@@ -9,6 +9,6 @@ app.get('/', (req, res)=>{
     res.render('home');
 })
 
-app.listen(3000, ()=>{
-    console.log('App is listening to port 3000.')
+app.listen(8080, ()=>{
+    console.log('App is listening to port.')
 })
